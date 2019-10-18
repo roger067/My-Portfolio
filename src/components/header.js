a28334ef89ca7e42728685687acdfee4d8872b76
Rogério Moura <21602077@aluno.cotemig.com.br>
2019-10-18 18:48:22 -0300

    starting to make info head

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,27 @@
 import React from "react"
+import logo from "../images/gatsby-astronaut.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faFacebookF,
+  faGithubAlt,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons"
 
-const Header = () => <div className="sidebar"></div>
+const Header = () => (
+  <div className="sidebar">
+    <img src={logo} alt="profile" />
+    <div className="info-top">
+      <h2>
+        <span>Rogério</span> Moura
+      </h2>
+      <div className="pill">Front-end Developer</div>
+      <div>
+        <FontAwesomeIcon icon={faFacebookF} />
+        <FontAwesomeIcon icon={faGithubAlt} />
+        <FontAwesomeIcon icon={faLinkedinIn} />
+      </div>
+    </div>
+  </div>
+)
 
 export default Header
